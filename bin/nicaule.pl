247caeb7fb378f944824983f8d32b5d259765caa
ligne <matt.boyle@gmail.com>
2011-10-24 19:02:19 +0100

    encapsulate the test-many-branches interface.

--- bin/nicaule.pl
+++ bin/nicaule.pl
@@ -8,72 +8,37 @@ use warnings;
 
 use 5.10.0;
 
-use Git;
-use File::Spec ();
-
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 
+use Git;
+
 use Sheba::Tester;
+use Sheba::Repository;
 
 
-# returns a config hash thing
-sub load_config
-{
-    return {
-        branches => [
-            'origin/master',
-        ],
-        parrot_upstream => 'git://github.com/parrot/parrot.git',
-        parrot_clone    => '/home/local/mlb/.smokers/parrot1',
-
-        #use_tmp => 1,
-        tmpdir => '/dev/shm',
-    };
-}
-
-
-# return a handle to a copy of the repository for running tests in.
-#
-# FIXME this is a terrible name.  it should hint that a chdir() will happen.
-sub repository
-{
-    my ($dir) = @_;
-
-    chdir $dir or die "Unable to change to '$dir': $!";
-
-    return Git->repository(DIRECTORY => $dir);
-}
-
-
-# return a list of branches to test.
-#
-# FIXME work out what branches have changed, either since last check, or $n
-# hours
-sub get_branches { return @{(shift)->{branches}} }
+my @branches = (
+    'origin/master',
+);
 
 
 sub main
 {
     my ($branch) = @_;
 
-    # load the config
-    my $config = load_config();
+    my $tester = Sheba::Tester->new();
 
     # get the test repository, and move there
-    my $repo = repository($config->{parrot_clone});
-
-    # get the branches to test
-    my @branches = get_branches($config);
-
-    # make sure the repository is up-to-date
-    $repo->command('fetch');
+    my $repo = Sheba::Repository->new();
+    $repo->prepare_repo;
 
     foreach my $b (@branches) {
-        $repo->command(qw( checkout -q ), $b);
-        my $tester = Sheba::Tester->new();
+        $repo->command(qw( checkout -q --force ), $b);
         $tester->run_tests();
     }
+
+    # need to chdir out of the tempdir, or it can't be cleaned up
+    chdir '/';
 
     return 0;
 }
